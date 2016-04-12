ddbfb87647d419e980cbf69efab17bfee35a4622
zihanghuang <zihang.huang@drake.edu>
2016-04-13 06:33:42 +0800

    Assignment2

---
+++ Assignment2.py
@@ -0,0 +1,14 @@
+# name: Zihang Huang
+# date: 2/2/2016
+# description: Credit given to www.sthurlow.com
+#               This code demonstrate how to
+#               create a list, display items of a list,
+#               adding items to a list and delete items of a list
+
+cats=['Tom','Snappy', 'Kitty', 'Jessie', 'Chester']
+print(cats)
+print(cats[1])
+cats.append('Catherine')
+print(cats)
+del cats[0]
+print(cats)
