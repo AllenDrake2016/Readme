15ddc8134f716564ae27fb1e34334b8888724437
zihanghuang <zihang.huang@drake.edu>
2016-04-13 23:46:25 +0800

    Lab 08 (2)

---
+++ MovieDriver.py
@@ -0,0 +1,19 @@
+# name: (Zihang Huang)
+# date: 4/8/2016
+# description: Lab #6 -- Classes and Objects
+
+
+#Sec1&Challenge1
+from Movie import*
+
+def main():
+    movie1 = Movie("Jurasstic Park",1993,"Steven Spielberg")
+    movie2 = Movie("Avatar", 2009, "James Cameron")
+    movie3 = Movie("Batman vs Superman", 2015,"Zack Snyder" )
+
+    print(movie1)
+    print(movie2)
+    print(movie3)
+
+main()
+
