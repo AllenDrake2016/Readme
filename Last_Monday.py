dd59272cc2c890a019e05a0f3d4107026d91b32a
zihanghuang <zihang.huang@drake.edu>
2016-05-09 18:33:18 +0800

    Last_Monday

---
+++ Last_Monday.py
@@ -0,0 +1,45 @@
+# def main():
+#     message(5)
+#     print("Hi")
+#
+# def message(n):
+#     if n>0:
+#         print('hello world',n)
+#         message(n-1)
+#
+# main()
+
+# def main():
+#     val=5
+#     show_triple(val)
+#
+# def show_triple(number):
+#     result=number*3
+#     print(result)
+#
+# main()
+
+
+# def main():
+#     print_star(8)
+#
+# def print_star(n):
+#     if n>0:
+#         print('*',end='')
+#         print_star(n-1)
+#
+# main()
+
+def main():
+    a=int(input("Enter a number")),
+    b=int(input("Enter another number"))
+    c=product(a,b)
+    print(c)
+
+def product(num1,num2):
+    result=num1+num2
+    return result
+
+
+
+main()
