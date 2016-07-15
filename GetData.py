3b2405f2629d01023f5b0d3e76af8d984ece4701
zihanghuang <zihang.huang@drake.edu>
2016-07-15 18:50:03 -0400

    Get_data_1.py

--- GetData.py
+++ GetData.py
@@ -1,166 +1,159 @@
-# infile = open("RowData.txt","r")
-# outfile = open("BreaksToLines.txt","w")
-#
-# while True:
-#     word1 = infile.readline(1)
-#     # print(word1)
-#     if word1=="<":
-#         outfile.write("\n"+word1)
-#     elif not word1:
-#         print("End")
-#         break
-#     else:
-#         outfile.write(word1)
-#         print(word1)
-#
-# infile.close()
-# outfile.close()
-#
-# infile=open("BreaksToLines.txt","r",encoding="ascii", errors="surrogateescape")
-# outfile=open("Dataout.txt","w",encoding="ascii", errors="surrogateescape")
-#
-#
-# while True:
-#     line_BTL_1=infile.readline()
-#     store_line_BTL_1=line_BTL_1
-#     print(store_line_BTL_1)
-#     break_to_words=""
-#     if len(store_line_BTL_1)>=6:
-#         for n in range(6):
-#             break_to_words+=store_line_BTL_1[n]
-#         # print(break_to_words)
-#     if not line_BTL_1:
-#         print("End")
-#         break
-#     elif break_to_words == '<input':
-#         outfile.write(store_line_BTL_1)
-#
-# infile.close()
-# outfile.close()
-#
-# infile = open("Dataout.txt","r")
-# outfile = open("BreaksToLines_Second_Degree.txt","w")
-#
-# while True:
-#     word1 = infile.readline(1)
-#     # print(word1)
-#     if word1=='"':
-#         outfile.write("\n"+word1)
-#     elif not word1:
-#         print("End")
-#         break
-#     else:
-#         outfile.write(word1)
-#         print(word1)
-#
-# infile.close()
-# outfile.close()
+####################################################### RowData to FirstDegree
+infile = open("RowData.txt","r")
+outfile = open("BreakToLines_FirstDegree.txt","w")
 
-
-infile=open("BreaksToLines_Second_Degree.txt","r")
-outfile=open("Tidy_data_first_drgree.txt","w")
-
-break_to_word_BTL_F_D_1="UDF"
-break_to_word_BTL_F_D_2="UDF"
 while True:
-    line_BTL_F_D_1=infile.readline()
-    store_line_BTL_F_D_1=line_BTL_F_D_1
-    break_to_word_BTL_F_D_1=""
-    #print(store_line_BTL_F_D_1)
-
-
-
-    if len(store_line_BTL_F_D_1)>=8:
-        for n in range(8):
-            break_to_word_BTL_F_D_1+=store_line_BTL_F_D_1[n]
-    #print(break_to_word_BTL_F_D_1)
-
-
-
-    if break_to_word_BTL_F_D_1=='"rpDefen':
-        outfile.write(store_line_BTL_F_D_1)
-        print(store_line_BTL_F_D_1)
-
-
-    elif break_to_word_BTL_F_D_2=='" value=':
-        outfile.write(store_line_BTL_F_D_2)
-        outfile.write(store_line_BTL_F_D_1)
-        #print(store_line_BTL_F_D_1)
-
-    elif not line_BTL_F_D_1:
+    line_BTL_FD = infile.readline()
+    words=line_BTL_FD.split()
+    # print(words)
+    for word in words:
+        store_word=word+"\n"
+        outfile.write(store_word)
+    if not line_BTL_FD:
         print("End")
         break
-
-    store_line_BTL_F_D_2=line_BTL_F_D_1
-    if len(line_BTL_F_D_1)>=8:
-        break_to_word_BTL_F_D_2=break_to_word_BTL_F_D_1
-    else:
-        break_to_word_BTL_F_D_2=""
-
 
 infile.close()
 outfile.close()
 
 
+####################################################### FirstDegree to SecondDegree
+infile=open("BreakToLines_FirstDegree.txt","r")
+outfile=open("BreakToLines_SecondDegree.txt","w")
 
-
-
-
-infile=open("BreaksToLines_Second_Degree.txt","r")
-outfile=open("Tidy_data_second_drgree.txt","w")
-
-break_to_word_BTL_S_D_1="UDF"
-break_to_word_BTL_S_D_2="UDF"
+break_to_words_BTL_SD_1="UDF"
 while True:
-    line_BTL_S_D_1=infile.readline()
-    store_line_BTL_S_D_1=line_BTL_S_D_1
-    break_to_word_BTL_S_D_1=""
-    #print(store_line_BTL_S_D_1)
+    line_BTL_SD_1=infile.readline()
+    store_line_BTL_SD_1=line_BTL_SD_1
+    break_to_words_BTL_SD_1=""
+    #print(store_line_BTL_SD_1)
+
+    if len(store_line_BTL_SD_1)>=24:
+        for n in range(24):
+            break_to_words_BTL_SD_1+=store_line_BTL_SD_1[n]
+        #print(break_to_words_BTL_SD_1)
+        if break_to_words_BTL_SD_1 != 'class="cptLeftAlignBold"':
+            break_to_words_BTL_SD_1=""
+            for n in range(8):
+                break_to_words_BTL_SD_1+=store_line_BTL_SD_1[n]
+            #print(break_to_words_BTL_SD_1)
 
 
 
-    if len(store_line_BTL_S_D_1)>=8:
+    if break_to_words_BTL_SD_1 == 'class="cptLeftAlignBold"' or break_to_words_BTL_SD_1 =='onchange':
+        #print(break_to_words_BTL_SD_1)
+        line_BTL_SD_2=infile.readline()
+        line_BTL_SD_3=infile.readline()
+        outfile.write(line_BTL_SD_1)
+        outfile.write(line_BTL_SD_2)
+        outfile.write(line_BTL_SD_3)
+
+    elif not line_BTL_SD_1:
+        print("End")
+        break
+infile.close()
+outfile.close()
+
+####################################################### SecondDegree to ThirdDegree
+infile=open("BreakToLines_SecondDegree.txt","r")
+outfile=open("BreakToLines_ThirdDegree.txt","w")
+
+break_to_words_BTL_TD_1="UDF"
+break_to_words_BTL_TD_2="UDF"
+break_to_words_BTL_TD_3="UDF"
+while True:
+    line_BTL_TD_1 = infile.readline()
+    store_line_BTL_TD_1=line_BTL_TD_1
+    break_to_words_BTL_TD_1=""
+
+    if len(store_line_BTL_TD_1)>=8:
         for n in range(8):
-            break_to_word_BTL_S_D_1+=store_line_BTL_S_D_1[n]
-    #print(break_to_word_BTL_S_D_1)
+            break_to_words_BTL_TD_1+=store_line_BTL_TD_1[n]
+        #print(break_to_words_BTL_TD_1)
 
 
+    if break_to_words_BTL_TD_1 == 'onchange':
+        line_BTL_TD_2=infile.readline()
+        line_BTL_TD_3=infile.readline()
 
-    if break_to_word_BTL_S_D_1=='"rpDefen':
-        #print(store_line_BTL_S_D_1)
-        break_to_word_BTL_S_D_1=""
-        for n in range(26):
-            break_to_word_BTL_S_D_1+=store_line_BTL_S_D_1[n]
-        #print(break_to_word_BTL_S_D_1)
-        # if break_to_word_BTL_S_D_1=='"rpDefendantInformation_tb' or break_to_word_BTL_S_D_1=='"rpDefendantInformation$cb':
-        #     outfile.write(line_BTL_S_D_1)
-        #     #print(break_to_word_BTL_S_D_1)
+        if len(line_BTL_TD_2)>=4:
+            break_to_words_BTL_TD_2=""
+            for n in range(4):
+                break_to_words_BTL_TD_2+=line_BTL_TD_2[n]
+            #print(break_to_words_BTL_TD_3)
+            if break_to_words_BTL_TD_2=="type":
+                line_BTL_TD_2="NA"
 
-        if break_to_word_BTL_S_D_1=='"rpDefendantInformation_tb':
-            store_line_BTL_S_D_1=store_line_BTL_S_D_1.strip('"rpDefendantInformation_tb')
-            outfile.write(store_line_BTL_S_D_1)
-            #print(break_to_word_BTL_S_D_1)
-        if break_to_word_BTL_S_D_1=='"rpDefendantInformation$cb':
-            print(store_line_BTL_S_D_1)
-            store_line_BTL_S_D_1=store_line_BTL_S_D_1.strip('"rpDefendantInformation$cbo')
-            outfile.write(store_line_BTL_S_D_1)
-            print(store_line_BTL_S_D_1)
+        if len(line_BTL_TD_3)>=4:
+            break_to_words_BTL_TD_3=""
+            for n in range(4):
+                break_to_words_BTL_TD_3+=line_BTL_TD_3[n]
+            #print(break_to_words_BTL_TD_3)
+            if break_to_words_BTL_TD_3=="type":
+                line_BTL_TD_3="\n"
+        line_BTL_TD_2=line_BTL_TD_2.strip("\n")
+        output_line=line_BTL_TD_2+","+line_BTL_TD_3
+        outfile.write(output_line)
+
+    elif len(store_line_BTL_TD_1)>=24:
+        break_to_words_BTL_TD_1=""
+        for n in range(24):
+            break_to_words_BTL_TD_1+=store_line_BTL_TD_1[n]
+        #print(break_to_words_BTL_TD_1)
+
+        if break_to_words_BTL_TD_1=='class="cptLeftAlignBold"':
+            line_BTL_TD_2=infile.readline()
+            line_BTL_TD_3=infile.readline()
+            line_BTL_TD_2=line_BTL_TD_2.strip("\n")
+            output_line=line_BTL_TD_2+line_BTL_TD_3
+            outfile.write(output_line)
 
 
-    elif break_to_word_BTL_S_D_2=='" value=':
-        outfile.write(store_line_BTL_S_D_1)
-        #print(store_line_BTL_S_D_1)
-
-    elif not line_BTL_S_D_1:
+    elif not line_BTL_TD_1:
         print("End")
         break
 
-    store_line_BTL_S_D_2=line_BTL_S_D_1
-    if len(line_BTL_S_D_1)>=8:
-        break_to_word_BTL_S_D_2=break_to_word_BTL_S_D_1
-    else:
-        break_to_word_BTL_S_D_2=""
-
-
 infile.close()
 outfile.close()
+
+####################################################### ThirdDegree to FourthDegree
+infile=open("BreakToLines_ThirdDegree.txt","r")
+outfile=open("BreakToLines_FourthDegree.txt","w")
+break_to_words_D_1="UDF"
+count=0
+while True:
+    count+=1
+    line_D_1 = infile.readline()
+    if count%2==0:
+        for n in range(2):
+            break_to_words_D_1+=line_D_1[n]
+        line_D_1=line_D_1.replace("value=","")
+        #print(line_D_1)
+        line_D_1=line_D_1.replace('"','')
+        #print(line_D_1)
+        line_D_1=line_D_1.replace(',',' ')
+        #print(line_D_1)
+        if break_to_words_D_1=="NA":
+            line_D_1=break_to_words_D_1+"\n"
+        outfile.write(line_D_1)
+
+    elif not line_D_1:
+        print("End")
+        break
+    break_to_words_D_1=""
+infile.close()
+outfile.close()
+
+####################################################### FourthDegree to Database
+import csv
+infile=open("BreakToLines_FourthDegree.txt","r")
+rows = [line.split() for line in infile]
+print(rows)
+with open("Experiment.csv", "a") as outfile:
+    writer = csv.writer(outfile, lineterminator=',')
+    for val in rows:
+        writer.writerow([val])
+        print(val)
+    writer = csv.writer(outfile, lineterminator='')
+    writer.writerow("\n")
+infile.close()
