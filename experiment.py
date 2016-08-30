c60925bcc6858aa65f6c4658380ec194fe447658
AllenDrake2016 <zihang.huang@drake.edu>
2016-08-30 18:18:07 -0400

    Crawler_experiment.py

---
+++ experiment.py
@@ -0,0 +1,11 @@
+# links=[1,2,3,4,5,6,7,8,9,0]
+# queue=[1]
+# crawled=[2]
+# for url in links:
+#     if url in queue:
+#         continue
+#     if url in crawled:
+#         continue
+#     print(links)
+
+####################################################### main
