ce99d3df6c06ce1d1962549f4791a069230d95be
AllenDrake2016 <zihang.huang@drake.edu>
2016-09-23 13:32:37 -0400

    Work_Experiment_Class_Experiment.py

---
+++ Class_Experiment.py
@@ -0,0 +1,13 @@
+# name: (Zihang Huang)
+# date: 7/22/2016
+# description: inheritance and polymorphism
+
+import Html_Broswer
+
+def main():
+    pub1=Html_Broswer.Publication("Prof.Urness' Ph.D. disseration", 119)
+    book1=Html_Broswer.Book("Green Eggs and Ham", 62,987)
+    print(pub1)
+    print(book1)
+
+main()
